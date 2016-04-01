cd82975b00de55536b95dfbe5522f48c73313f73
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-04-01 14:19:21 -0600

    created rooms with unique socket

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,2 @@
 console.log('hi');
 var dog = 'dog';
-
-  $('#msg').submit(function(){
-    socket.emit(dog, { room: 1, message: $('#m').val()});
-    $('#m').val('');
-    return false;
-  });
-  socket.on(dog, function(msg){
-    $('#messages').append($('<li>').text(msg.message));
-  });
